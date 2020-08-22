f00affc845f918c58a68e41791870a42e283c5ae
Anton Zotin <toxin88@mail.ru>
2020-08-22 22:29:04 +0300

    fix

--- main.py
+++ main.py
@@ -14,7 +14,7 @@
 
 # [START gae_python38_app]
 from flask import Flask
-import stocker
+# import stocker
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
