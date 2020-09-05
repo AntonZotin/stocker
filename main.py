0d4af8dd6bb751bc404955f76f94aa9c12a227c0
Anton Zotin <toxin88@mail.ru>
2020-09-05 21:09:57 +0300

    fix

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ MESSAGES = []
 TOKENS = []
 CLAIMS = []
 
+
 @app.route('/')
 def hello():
     s = stocker.predict.tomorrow('AAPL')
@@ -52,26 +53,3 @@ if __name__ == '__main__':
     # application on Google App Engine. See entrypoint in app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
 # [END app]
-
-# # [START gae_python38_app]
-# from flask import Flask
-# import stocker
-#
-# # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
-# # called `app` in `main.py`.
-# app = Flask(__name__)
-#
-#
-# @app.route('/')
-# def hello():
-#     s = stocker.predict.tomorrow('AAPL')
-#     res = f'Result for {s[2]} {s[0]}'
-#     return res
-#
-#
-# if __name__ == '__main__':
-#     # This is used when running locally only. When deploying to Google App
-#     # Engine, a webserver process such as Gunicorn will serve the app. This
-#     # can be configured by adding an `entrypoint` to app.yaml.
-#     app.run(host='127.0.0.1', port=8080, debug=True)
-# # [END gae_python38_app]
