3b433f395499a5f1b84edd7f466a7cc006c33c1f
Anton Zotin <toxin88@mail.ru>
2020-09-06 21:35:14 +0300

    fix

--- app.py
+++ app.py
@@ -1,25 +1,39 @@
+import _thread
 import os
-import logging
+import time
 
-from flask import Flask
+from flask import Flask, render_template
 
-# Change the format of messages logged to Stackdriver
-logging.basicConfig(format='%(message)s', level=logging.INFO)
+import stocker
 
 app = Flask(__name__)
+
+data = {}
 
 
 @app.route('/')
 def home():
-	try:
-		import stocker
-		s = stocker.predict.tomorrow('AAPL')
-		res = f'Result for {s[2]} {s[0]}'
-		return res
-	except Exception as e:
-		logging.error(e)
-		return str(e)
+    res = []
+    for date, values in data.items():
+        res.append({
+            'date': date,
+            'min': f'{min(values["values"])} ({min(values["errors"])})',
+            'avg': f'{round(sum(values["values"]) / len(values["values"]), 2)} ({round(sum(values["errors"]) / len(values["errors"]), 2)})',
+            'max': f'{max(values["values"])} ({max(values["errors"])})'
+        })
+    return render_template('index.html', data=res)
+
+
+def get_data():
+    s = stocker.predict.tomorrow('AAPL')
+    if s[2] in data:
+        data[s[2]]['values'].append(s[0])
+        data[s[2]]['errors'].append(s[1])
+    else:
+        data[s[2]] = {'values': [s[0]], 'errors': [s[1]]}
+    time.sleep(3600)
 
 
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    _thread.start_new_thread(get_data, ())
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
