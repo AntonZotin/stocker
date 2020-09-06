4d24999217015563d9c19681f7a4fa4cbb504748
Anton Zotin <toxin88@mail.ru>
2020-09-06 19:21:42 +0300

    fix

--- app.py
+++ app.py
@@ -2,28 +2,20 @@ import os
 import logging
 
 from flask import Flask
+import stocker
 
 # Change the format of messages logged to Stackdriver
 logging.basicConfig(format='%(message)s', level=logging.INFO)
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-	html = """
-<html>
- <head>
-  <title>
-   Google Cloud Run - Sample Python Flask Example
-  </title>
- </head>
- <body>
-  <p>Hello Google Cloud Run World!</p>
-  <a href="https://cloud.google.com/run/" target="_blank">Google Cloud Run Website</a>
- </body>
-</html>
-"""
-	return html
+	s = stocker.predict.tomorrow('AAPL')
+	res = f'Result for {s[2]} {s[0]}'
+	return res
+
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
